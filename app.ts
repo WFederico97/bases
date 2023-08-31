0f7710bb3be6576fde7e6a6dd23e0fec50cd0cbc
WFederico97 <wuthrich09@gmail.com>
2023-08-30 21:55:17 -0300

    First Commit

---
+++ app.ts
@@ -0,0 +1,11 @@
+//! Any = todo tipo de datos, number = numerico, string = cadena, bool = T o F
+//*Funcion anonima autoinvocada para que los archivos no choquen entre si
+(() => {
+    const a: number = 10;
+    console.log(a)
+})()
+
+
+// function sayHello(msg: string){
+//     console.log(msg)
+// }
