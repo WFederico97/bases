a090d29bab00cbb341c6db21dcc9e8f0036f812d
Federico W <wuthrich09@gmail.com>
2023-09-21 18:29:38 -0300

    SECCION-11

---
+++ ts-proyecto/src/data/powers.ts
@@ -0,0 +1,20 @@
+interface Power {
+    id: number;
+    desc: string,
+}
+
+const powers: Power[] = [
+    {
+        id: 1,
+        desc: 'Super Fuerza'
+    },
+    {
+        id: 2,
+        desc: 'Super Velocidad'
+    },
+    {
+        id: 3,
+        desc: 'Invisibilidad'
+    },
+]
+export default powers
---
+++ ts-proyecto/src/generics/generics.ts
@@ -0,0 +1,15 @@
+export const printObj = (argument: any ) :void => {
+    console.log(argument)
+}
+
+// export function genericFn (argument: any) {
+//     return argument;
+// }
+
+export function genericFn<T> (argument: T) { //! con <T> designamos que una funcion es genérica
+    return argument;
+}
+
+export const genericArrowFn = <T> (argument: T) => {
+    return argument
+}
